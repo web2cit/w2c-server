7bfe982a0d1f1d018c093bc7e8d72f8d29d52785
Diego de la Hera <delahera@gmail.com>
2022-02-23 17:44:15 -0300

    Initial working prototype

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,39 @@
 import express from "express";
-import * as W2C from "web2cit";
-// import { Domain } from 'web2cit';
+import { Domain, Webpage, fallbackTemplate } from "web2cit";
 
 const app = express();
-const port = 3000;
-app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
+const port = process.env.PORT || 3000;
+
+app.get("/:url(*)", (req, res) => {
+  // handle wrong urls
+  const url = new URL(req.params.url);
+  const target = new Webpage(url.href);
+  const domain = new Domain(
+    target.domain,
+    undefined,
+    // fixme: change the order here
+    fallbackTemplate
+  );
+
+  // consider having an init method
+  domain.templates
+    .getLatestRevision()
+    .then((revision) => {
+      if (revision !== undefined) {
+        return domain.templates.loadConfiguration(revision.configuration);
+      }
+      return Promise.resolve();
+    })
+    // I don't like this
+    // May be solved with the init method proposed above
+    .then(() => {
+      return domain.translate(target);
+    })
+    .then((output) => {
+      res.send(output);
+    });
 });
+
 app.listen(port, () => {
-  // if (err) {
-  //   return console.error(err);
-  // }
-  const myDomain = new W2C.Domain("test");
-  console.log(`Domain object for "${myDomain.domain}" successfully created!"`);
-  return console.log(`server is listening on ${port}!`);
+  console.log(`server is listening on ${port}!`);
 });
--- src/types.d.ts
+++
@@ -1,1 +0,0 @@
-declare module "web2cit";
