0453b78b3801be0a10ab2077b2a0e547caa85605
Diego de la Hera <delahera@gmail.com>
2022-02-25 17:45:23 -0300

    Improve HTML output

--- src/app.ts
+++ src/app.ts
@@ -32,16 +32,65 @@ app.get("/:url(*)", (req, res) => {
     .then((output) => {
       // may be undefined!
       const citation = output.translation.outputs[0].citation;
-      // how do I change the URL interpreted by zotero? use a canonical?
+      const metaTags: string[] = [];
+      const items: string[] = [];
+      (Object.keys(citation) as Array<keyof typeof citation>).forEach(
+        (field) => {
+          if (field === "key" || field === "version") return;
+
+          let prefix = "z";
+          const contents: string[] = [];
+
+          if (field === "tags") {
+            const tags = citation[field];
+            if (tags === undefined) return;
+            tags.forEach((tag) => contents.push(tag.tag));
+          } else {
+            const value = citation[field];
+            if (value === undefined) return;
+
+            if (typeof value === "string") {
+              contents.push(value);
+            } else {
+              value.forEach((item) => {
+                if (typeof item === "string") {
+                  contents.push(item);
+                } else {
+                  const creator = item.join(", ");
+                  contents.push(creator);
+                  // fixme: support creators other than author
+                  prefix = "so";
+                }
+              });
+            }
+          }
+          contents.forEach((content) => {
+            const tag = `<meta property="${prefix}:${field}" content="${content}"/>`;
+            metaTags.push(tag);
+            const item = `<li><b>${field}s:</b> ${content}</li>`;
+            items.push(item);
+          });
+        }
+      );
+      // fixme: publisher mapped to multiple fields ends in extra
+      // fixme: source ends in extra. what does zotero use it for?
       res.send(`
 <!DOCTYPE html>
+<html
+  xmlns="http://www.w3.org/1999/xhtml"
+  prefix="z:http://www.zotero.org/namespaces/export#"
+>
 <head>
   <link rel="canonical" href="${citation.url}" />
-  <meta property="og.title" content="${citation.title}">
+  ${metaTags.join("")}
 </head>
 <body>
-  <h1>${citation.title}</h1>
+  <p>Web2Cit translation for ${url}:</p>
+  <ul>
+  ${items.join("")}
+  </ul>
 </body>
+</html>
         `);
     });
 });
