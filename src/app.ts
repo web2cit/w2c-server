15d24b0695e9451a85cf4d7308ff8fc8ac8f362a
Diego de la Hera <delahera@gmail.com>
2022-02-27 18:59:33 -0300

    use async handler

--- src/app.ts
+++ src/app.ts
@@ -1,81 +1,101 @@
-import express from "express";
+import express, { NextFunction, RequestHandler } from "express";
 import { Domain, Webpage, fallbackTemplate } from "web2cit";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/:url(*)", (req, res) => {
-  // handle wrong urls
-  const url = new URL(req.params.url);
-  const target = new Webpage(url.href);
-  const domain = new Domain(
-    target.domain,
-    undefined,
-    // fixme: change the order here
-    fallbackTemplate
-  );
+// TODO: won't be needed anymore with Express v5
+// A wrapper function.
+// It takes fn as parameter, with fn an async version of a RequestHandler:
+// it takes the same parameters (...args: Parameters<RequestHandler>),
+// but returns a promise (Promise<ReturnType<RequestHandler>>).
+// It returns a wrapped function (wrappedFn)
+// that takes the same parameters as a RequestHandler (req, res. next)
+// and returns a promise (Promise<ReturnType<RequestHandler>>)
+// with its reject value sent to the "next" function
+// see https://expressjs.com/en/advanced/best-practice-performance.html#use-promises
+function wrap(
+  fn: (
+    ...args: Parameters<RequestHandler>
+  ) => Promise<ReturnType<RequestHandler>>
+) {
+  const wrappedFn = function (
+    ...args: Parameters<RequestHandler>
+  ): Promise<ReturnType<RequestHandler>> {
+    const [req, res, next] = args;
+    return fn(req, res, next).catch((reason) => {
+      next(reason);
+    });
+  };
+  return wrappedFn;
+}
 
-  // consider having an init method
-  domain.templates
-    .getLatestRevision()
-    .then((revision) => {
-      if (revision !== undefined) {
-        return domain.templates.loadConfiguration(revision.configuration);
-      }
-      return Promise.resolve();
-    })
-    // I don't like this
-    // May be solved with the init method proposed above
-    .then(() => {
-      return domain.translate(target);
-    })
-    .then((output) => {
-      // may be undefined!
-      const citation = output.translation.outputs[0].citation;
-      const metaTags: string[] = [];
-      const items: string[] = [];
-      (Object.keys(citation) as Array<keyof typeof citation>).forEach(
-        (field) => {
-          if (field === "key" || field === "version") return;
+app.get(
+  "/:url(*)",
+  wrap(async (req, res) => {
+    // handle wrong urls
+    const url = new URL(req.params.url);
+    const target = new Webpage(url.href);
+    const domain = new Domain(
+      target.domain,
+      undefined,
+      // fixme: change the order here
+      fallbackTemplate
+    );
 
-          let prefix = "z";
-          const contents: string[] = [];
+    // consider having an init method
+    const revision = await domain.templates.getLatestRevision();
+    if (revision !== undefined) {
+      domain.templates.loadConfiguration(revision.configuration);
+    }
 
-          if (field === "tags") {
-            const tags = citation[field];
-            if (tags === undefined) return;
-            tags.forEach((tag) => contents.push(tag.tag));
-          } else {
-            const value = citation[field];
-            if (value === undefined) return;
+    const output = await domain.translate(target);
 
-            if (typeof value === "string") {
-              contents.push(value);
+    // fixme: may be undefined!
+    const citation = output.translation.outputs[0].citation;
+    const metaTags: string[] = [];
+    const items: string[] = [];
+    (Object.keys(citation) as Array<keyof typeof citation>).forEach((field) => {
+      if (field === "key" || field === "version") return;
+
+      let prefix = "z";
+      const contents: string[] = [];
+
+      if (field === "tags") {
+        const tags = citation[field];
+        if (tags === undefined) return;
+        tags.forEach((tag) => contents.push(tag.tag));
+      } else {
+        const value = citation[field];
+        if (value === undefined) return;
+
+        if (typeof value === "string") {
+          contents.push(value);
+        } else {
+          value.forEach((item) => {
+            if (typeof item === "string") {
+              contents.push(item);
             } else {
-              value.forEach((item) => {
-                if (typeof item === "string") {
-                  contents.push(item);
-                } else {
-                  item.reverse();
-                  const creator = item.join(", ");
-                  contents.push(creator);
-                  // fixme: support creators other than author
-                  prefix = "so";
-                }
-              });
+              item.reverse();
+              const creator = item.join(", ");
+              contents.push(creator);
+              // fixme: support creators other than author
+              prefix = "so";
             }
-          }
-          contents.forEach((content) => {
-            const tag = `<meta property="${prefix}:${field}" content="${content}"/>`;
-            metaTags.push(tag);
-            const item = `<li><b>${field}:</b> ${content}</li>`;
-            items.push(item);
           });
         }
-      );
-      // fixme: publisher mapped to multiple fields ends in extra
-      // fixme: source ends in extra. what does zotero use it for?
-      res.send(`
+      }
+      contents.forEach((content) => {
+        const tag = `<meta property="${prefix}:${field}" content="${content}"/>`;
+        metaTags.push(tag);
+        const item = `<li><b>${field}:</b> ${content}</li>`;
+        items.push(item);
+      });
+    });
+
+    // fixme: publisher mapped to multiple fields ends in extra
+    // fixme: source ends in extra. what does zotero use it for?
+    res.send(`
 <!DOCTYPE html>
 <html
   xmlns="http://www.w3.org/1999/xhtml"
@@ -92,9 +112,9 @@ app.get("/:url(*)", (req, res) => {
   </ul>
 </body>
 </html>
-        `);
-    });
-});
+  `);
+  })
+);
 
 app.listen(port, () => {
   console.log(`server is listening on ${port}!`);
